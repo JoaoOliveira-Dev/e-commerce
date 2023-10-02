288bbdc8a4389290bea10950af39e7d62dbb7fe9
MrWatchman <joaooli17@hotmail.com>
2023-10-02 13:39:13 -0300

    Front-end login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Login from "@/components/login/login";
+import Image from "next/image";
+import background from "../../assets/background.jpg";
+
+export default function login() {
+  return (
+    <main>
+      <Image className="background-image" src={background} alt="Background" />
+      <Login />
+    </main>
+  );
+}
---
+++ src/components/login/login.tsx
@@ -0,0 +1,34 @@
+import "./login.css";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "../../assets/logo.png";
+import background from "../../assets/background.png";
+
+export default function Login() {
+  return (
+    <div className="login-box">
+      <Link href="/">
+        <Image className="logo" src={logo} alt="Logo" />
+      </Link>
+      {/* <h1>Faça seu login</h1> */}
+      <form>
+        <div className="email-box">
+          <input type="text" placeholder="Email" />
+        </div>
+        <div className="password-box">
+          <input type="password" placeholder="Senha" />
+        </div>
+        <div className="remember-box">
+          <input type="checkbox" />
+          <span>Lembrar-me</span>
+        </div>
+        <div className="box-buttons">
+          <button className="login-button">Entrar</button>
+          <button className="conta-button">Crie uma conta</button>
+        </div>
+      </form>
+    </div>
+  );
+}
